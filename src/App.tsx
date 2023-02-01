5719ff24c06e771d90a30d0c5fe76b0a46f27c9b
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2023-02-01 15:32:32 -0300

    Nodes and Edges of React Flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,58 @@
-import ReactFlow, { Background, Controls } from 'reactflow';
+import ReactFlow, { addEdge, Background, Connection, ConnectionMode, Controls, Node, useEdgesState } from 'reactflow';
 import { zinc } from 'tailwindcss/colors'
 import 'reactflow/dist/style.css';
 
+import { Square } from './components/nodes/Square';
+import { useCallback } from 'react';
+
+const NODE_TYPES = {
+  square: Square,
+
+}
+
+const INITIAL_NODES = [
+  {
+    id: crypto.randomUUID(),
+    type: 'square',
+    position: { 
+      x: 200, 
+      y: 400 
+    },
+    data: {
+
+    }
+  },
+
+  {
+    id: crypto.randomUUID(),
+    type: 'square',
+    position: { 
+      x: 1000, 
+      y: 400 
+    },
+    data: {
+
+    }
+  },
+] satisfies Node[]
+
 export function App() {
+  const [edges, setEdges, onEdgesChange] = useEdgesState([])
+
+  const onConnect = useCallback((connection: Connection) => {
+    return setEdges(edges => addEdge(connection, edges))
+  }, [])
+
   return (
     <div className='w-screen h-screen'>
-      <ReactFlow>
+      <ReactFlow
+        nodeTypes={NODE_TYPES}
+        nodes={INITIAL_NODES}
+        connectionMode={ConnectionMode.Loose}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      >
         <Background 
           gap={12}
           size={2}
