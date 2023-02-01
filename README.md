c6e0f96a6f3a6d3f3a9515f06dd77901fa89be60
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2023-02-01 15:09:31 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ui-figma-jam
+ Creating Figma Jam with React
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import ReactFlow from 'reactflow';
+import 'reactflow/dist/style.css';
+
+export function App() {
+  return (
+    <h1>
+      Salve
+    </h1>
+  )
+}
